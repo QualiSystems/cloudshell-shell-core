030cb0890f93f415b83b52dc2aa79cc111301e2d
Sergii Shmarkatiuk <sergey.shmarkatyuk@gmail.com>
2016-03-30 17:29:09 +0300

    Refactored CommandTemplateService to static class

--- cloudshell/shell/core/command_template/command_template_service.py
+++ cloudshell/shell/core/command_template/command_template_service.py
@@ -1,1 +1,57 @@
+import re
+from cloudshell.shell.core.command_template.command_template_validator import CommandTemplateValidator
 
+_TEMPLATE_DICT = {}
+
+def add_commands(commands):
+    _TEMPLATE_DICT.update(commands)
+
+class CommandTemplateService:
+
+    _command_templates = {}
+    _error_list = []
+
+    @staticmethod
+    def send_commands_list(commands_list, send_command_func=None):
+        if not send_command_func:
+            raise Exception("Need send command function")
+        output = ""
+        for command in commands_list:
+            output += send_command_func(command)
+        return output
+
+    @staticmethod
+    def execute_command_map(command_map, send_command_func=None):
+        """
+        Configures interface ethernet
+        :param kwargs: dictionary of parameters
+        :return: success message
+        :rtype: string
+        """
+
+        commands_list = CommandTemplateService.get_commands_list(command_map)
+        output = CommandTemplateService.send_commands_list(commands_list, send_command_func)
+        CommandTemplateService._check_output_for_errors(output)
+        return output
+
+    @staticmethod
+    def _check_output_for_errors(output):
+        for error_pattern in CommandTemplateService._error_list:
+            if re.search(error_pattern, output):
+                CommandTemplateService.rollback()
+                raise Exception(
+                    'Output contains error with pattern: "{0}", for output: "{1}"'.format(error_pattern, output))
+        return output
+
+    @staticmethod
+    def get_commands_list(command_map):
+        prepared_commands = []
+        for command, value in command_map.items():
+            if command in CommandTemplateService._command_templates:
+                command_template = CommandTemplateService._command_templates[command]
+                prepared_commands.append(CommandTemplateValidator.get_validate_list(command_template, value))
+        return prepared_commands
+
+    @staticmethod
+    def rollback():
+        CommandTemplateService.execute_command_map({'rollback': []})
