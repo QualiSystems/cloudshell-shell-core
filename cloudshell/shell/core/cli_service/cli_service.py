096c12c19914f356013c023cd8a6a6e2334d6ce8
Sergii Shmarkatiuk <sergey.shmarkatyuk@gmail.com>
2016-03-30 17:32:27 +0300

    Moved _check_output_for_errors to cli_service

--- cloudshell/shell/core/cli_service/cli_service.py
+++ cloudshell/shell/core/cli_service/cli_service.py
@@ -1,4 +1,14 @@
 from cloudshell.shell.core.cli_service.cli_service_interface import CliServiceInterface
+from cloudshell.shell.core.command_template.command_template_service import CommandTemplateService
+import re
 
 class CliService(CliServiceInterface):
-    pass
+
+    @staticmethod
+    def _check_output_for_errors(output):
+        for error_pattern in CommandTemplateService._error_list:
+            if re.search(error_pattern, output):
+                CommandTemplateService.rollback()
+                raise Exception(
+                    'Output contains error with pattern: "{0}", for output: "{1}"'.format(error_pattern, output))
+        return output
--- cloudshell/shell/core/command_template/command_template_service.py
+++ cloudshell/shell/core/command_template/command_template_service.py
@@ -34,14 +34,6 @@ class CommandTemplateService:
         CommandTemplateService._check_output_for_errors(output)
         return output
 
-    @staticmethod
-    def _check_output_for_errors(output):
-        for error_pattern in CommandTemplateService._error_list:
-            if re.search(error_pattern, output):
-                CommandTemplateService.rollback()
-                raise Exception(
-                    'Output contains error with pattern: "{0}", for output: "{1}"'.format(error_pattern, output))
-        return output
 
     @staticmethod
     def get_commands_list(command_map):
