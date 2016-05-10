924b7cd45889a4a2b9cb5c93d95a8d3654b3369f
Yaroslav Nikonorov <yaroslav.n@qualisystems.com>
2016-05-10 17:19:03 +0300

    Removed examples

--- cloudshell/shell/core/examples/test_driver/test_bootstrap.py
+++
@@ -1,7 +0,0 @@
-from cloudshell.shell.core.driver_bootstrap import DriverBootstrap
-
-
-class TestBootstrap(DriverBootstrap):
-
-    def configuration(self, binder):
-        binder.bind_to_provider('handler', self._config.HANDLER_CLASS)
--- cloudshell/shell/core/examples/test_driver/test_handler.py
+++
@@ -1,8 +0,0 @@
-import inject
-
-
-class TestHandler:
-    @inject.params(cli='cli_service')
-    def send_dd(self, cli=None):
-        out = cli.send_command('ls')
-        return out
