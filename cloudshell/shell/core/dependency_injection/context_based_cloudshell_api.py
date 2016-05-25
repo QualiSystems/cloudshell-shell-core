35b8584a853270be4d4bbf6007331d3b387e247f
CoYe <wiseman.kostya@gmail.com>
2016-05-25 13:10:35 +0300

    Added workaround for methods when reservation domain is empty

--- cloudshell/shell/core/dependency_injection/context_based_cloudshell_api.py
+++ cloudshell/shell/core/dependency_injection/context_based_cloudshell_api.py
@@ -18,15 +18,18 @@ def get_cloudshell_api(context):
 def _open_new_api_connection(context):
     if hasattr(context, 'connectivity') \
             and context.connectivity \
-            and context.connectivity.__class__.__name__ == 'ConnectivityContext' \
-            and hasattr(context, 'reservation') \
-            and context.reservation \
-            and hasattr(context.reservation, 'domain'):
+            and context.connectivity.__class__.__name__ == 'ConnectivityContext':
+
+        if hasattr(context, 'reservation') \
+                and context.reservation \
+                and hasattr(context.reservation, 'domain'):
+            domain = context.reservation.domain
+        else:
+            domain = 'Global'
 
         server_address = context.connectivity.server_address
         api_port = context.connectivity.cloudshell_api_port
         token = context.connectivity.admin_auth_token
-        domain = context.reservation.domain
         api = CloudShellAPISession(server_address, port=api_port, token_id=token, domain=domain)
         return api
     else:
