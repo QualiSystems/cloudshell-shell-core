6a93d7bdb1cf5e6e78e42756e7ea571c2d5b0a46
Roni Dover <roni.dover@gmail.com>
2016-08-01 01:33:26 +0300

    depdendencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
+echo Installing depdendencies
+
 if [ "${TRAVIS_BRANCH}" = "master" ]
 then
     pip install -r requirements.txt
     pip install cloudshell-automation-api>=7.0.0.0,<7.1.0.0
+    echo Installing depdendencies2
 else
     pip install -r requirements.txt --extra-index-url https://testpypi.python.org/simple
     pip install cloudshell-automation-api>=7.0.0.0,<7.1.0.0 --extra-index-url https://testpypi.python.org/simple
+    echo Installing depdendencies3
 fi
 
 pip install -r test_requirements.txt
 pip install coveralls
+echo Installing depdendencies4
+
