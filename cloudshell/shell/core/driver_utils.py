4eb209d30411bf94c39bc1ef408b08468fe686bf
Yaroslav Nikonorov <yaroslav.n@qualisystems.com>
2016-08-31 17:48:58 +0300

    Added decorator for called methods

--- cloudshell/shell/core/driver_utils.py
+++ cloudshell/shell/core/driver_utils.py
@@ -2,38 +2,27 @@ from threading import Event, Lock
 
 
 class GlobalLock(object):
-    def __init__(self):
-        self._event = Event()
-        self._event.set()
-        self._lock = Lock()
+    _EVENT = Event()
+    _EVENT.set()
+    _LOCK = Lock()
 
     @staticmethod
     def lock(func):
         def _wrap_lock_func(*args, **kwargs):
-            return func(*args, **kwargs)
-
+            with GlobalLock._LOCK:
+                GlobalLock._EVENT.wait()
+                try:
+                    GlobalLock._EVENT.clear()
+                    result = func(*args, **kwargs)
+                finally:
+                    GlobalLock._EVENT.set()
+            return result
         return _wrap_lock_func
 
-    def _wrap_lock(self, func):
-        def _wrap_func(*args, **kwargs):
-            if func.__name__ == '_wrap_lock_func':
-                with self._lock:
-                    self._event.wait()
-                    try:
-                        self._event.clear()
-                        result = func(*args, **kwargs)
-                    finally:
-                        self._event.set()
-            else:
-                self._event.wait()
-                result = func(*args, **kwargs)
+    @staticmethod
+    def member(func):
+        def _wrap_member_func(*args, **kwargs):
+            GlobalLock._EVENT.wait()
+            result = func(*args, **kwargs)
             return result
-
-        return _wrap_func
-
-    def __getattribute__(self, item):
-        attr = super(GlobalLock, self).__getattribute__(item)
-        if callable(attr) and not item.startswith('_'):
-            return self._wrap_lock(attr)
-        else:
-            return attr
+        return _wrap_member_func
