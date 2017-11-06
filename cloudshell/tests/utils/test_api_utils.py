e0171000d1a06cd7b3cfe0e582609a481911e5f7
alexquali <alexey.b@quali.com>
2017-11-06 15:42:38 +0200

    Fixed unittests

--- cloudshell/tests/utils/test_api_utils.py
+++ cloudshell/tests/utils/test_api_utils.py
@@ -1,18 +1,24 @@
-from unittest import TestCase
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from mock import MagicMock
+from unittest import TestCase
+
 from cloudshell.shell.core.api_utils import decrypt_password_from_attribute
-from cloudshell.shell.core.context import ResourceCommandContext, ResourceContextDetails
 
 
 class TestApiUtils(TestCase):
+    def setUp(self):
+        self.context = MagicMock()
+
     def test_decrypt_password_by_attribute_name(self):
         password = 'test_password'
         attribute_name = 'Password'
-        context = ResourceCommandContext()
-        context.resource = ResourceContextDetails()
-        context.resource.attributes = {attribute_name: password}
-        api = MagicMock()
-        api.DecryptPassword.return_value.Value = password
-        self.assertTrue(password == decrypt_password_from_attribute(api=api, context=context,
-                                                                    password_attribute_name=attribute_name))
 
+        api = MagicMock()
+        self.context.resource.attributes = {attribute_name: password}
+
+        api.DecryptPassword.return_value.Value = password
+        self.assertEqual(password, decrypt_password_from_attribute(api=api,
+                                                                   context=self.context,
+                                                                   password_attribute_name=attribute_name))
