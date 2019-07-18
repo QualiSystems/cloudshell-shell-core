592513fc7c9d63052478c0c2479e8accfeff0c55
Kyrylo Maksymenko <saklar13@gmail.com>
2019-07-18 15:34:01 +0300

    update travis

--- runtests.py
+++
@@ -1,8 +0,0 @@
-import nose
-import nose.config
-import sys
-from nose.plugins.manager import DefaultPluginManager
-c = nose.config.Config()
-c.plugins=DefaultPluginManager()
-if not nose.run(config=c):
-    sys.exit(1)
