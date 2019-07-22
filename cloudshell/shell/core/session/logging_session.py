9c1840495b2b9ad63b120e7bc7551e08a24a2c50
Yaroslav Nikonorov <yaroslav.n@quali.com>
2019-07-22 15:54:01 +0300

    Fixed difference

--- cloudshell/shell/core/session/logging_session.py
+++ cloudshell/shell/core/session/logging_session.py
@@ -7,80 +7,8 @@ from cloudshell.shell.core.context_utils import is_instance_of, get_reservation_
 INVENTORY = 'inventory'
 
 
-def get_execution_info(context):
-    """Aggregate information about execution server
-
-    :param context: ResourceCommandContext
-    :return: dict with aggregated info
-    """
-
-    reservation_info = {}
-    hostname = socket.gethostname()
-    reservation_info['Python version'] = platform.python_version()
-    reservation_info['Operating System'] = platform.platform()
-    reservation_info['Platform'] = platform.system()
-    reservation_info['Hostname'] = hostname
-
-    try:
-        reservation_info['IP'] = socket.gethostbyname(hostname)
-    except:
-        reservation_info['IP'] = "n/a"
-
-    try:
-        reservation_info['ReservationID'] = get_reservation_context_attribute('reservation_id', context)
-        reservation_info['Description'] = get_reservation_context_attribute('description', context)
-        reservation_info['EnviromentName'] = get_reservation_context_attribute('environment_name', context)
-        reservation_info['Username'] = get_reservation_context_attribute('owner_user', context)
-    except:
-        pass
-
-    return reservation_info
-
-
-def get_logger_for_context(context):
-    """
-    Create logger for context
-    :param context:
-    :return: the logger object
-    :rtype: logging.Logger
-    """
-    if is_instance_of(context, 'AutoLoadCommandContext'):
-        log_group = INVENTORY
-        resource_name = context.resource.name
-    elif is_instance_of(context, 'ResourceCommandContext'):
-        log_group = context.reservation.reservation_id if context.reservation else INVENTORY
-        resource_name = context.resource.name
-    elif is_instance_of(context, 'ResourceRemoteCommandContext'):
-        log_group = context.remote_reservation.reservation_id if context.remote_reservation else INVENTORY
-        resource_name = context.remote_endpoints[0].name
-    else:
-        raise Exception('get_logger_for_context', 'Unsupported command context provided {0}'.format(context))
-
-    exec_info = get_execution_info(context)
-    qs_logger = get_qs_logger(log_group=log_group, log_category='QS', log_file_prefix=resource_name)
-    log_execution_info(qs_logger, exec_info)
-    return qs_logger
-
-
-def get_logger_with_thread_id(context):
-    """
-    Create QS Logger for command context AutoLoadCommandContext, ResourceCommandContext
-    or ResourceRemoteCommandContext with thread name
-    :param context:
-    :return:
-    :rtype: logging.Logger
-    """
-    logger = get_logger_for_context(context)
-    child = logger.getChild(threading.currentThread().name)
-    for handler in logger.handlers:
-        child.addHandler(handler)
-    child.level = logger.level
-    for log_filter in logger.filters:
-        child.addFilter(log_filter)
-    return child
-
-
 class LoggingSessionContext(object):
+
     def __init__(self, context):
         """
         Initializes logger for context
@@ -89,13 +17,86 @@ class LoggingSessionContext(object):
         self.context = context
         self._logger = None
 
+    @staticmethod
+    def get_execution_info(context):
+        """Aggregate information about execution server
+
+        :param context: ResourceCommandContext
+        :return: dict with aggregated info
+        """
+
+        reservation_info = {}
+        hostname = socket.gethostname()
+        reservation_info['Python version'] = platform.python_version()
+        reservation_info['Operating System'] = platform.platform()
+        reservation_info['Platform'] = platform.system()
+        reservation_info['Hostname'] = hostname
+
+        try:
+            reservation_info['IP'] = socket.gethostbyname(hostname)
+        except:
+            reservation_info['IP'] = "n/a"
+
+        try:
+            reservation_info['ReservationID'] = get_reservation_context_attribute('reservation_id', context)
+            reservation_info['Description'] = get_reservation_context_attribute('description', context)
+            reservation_info['EnviromentName'] = get_reservation_context_attribute('environment_name', context)
+            reservation_info['Username'] = get_reservation_context_attribute('owner_user', context)
+        except:
+            pass
+
+        return reservation_info
+
+    @staticmethod
+    def get_logger_for_context(context):
+        """
+        Create logger for context
+        :param context:
+        :return: the logger object
+        :rtype: logging.Logger
+        """
+        if is_instance_of(context, 'AutoLoadCommandContext'):
+            log_group = INVENTORY
+            resource_name = context.resource.name
+        elif is_instance_of(context, 'ResourceCommandContext'):
+            log_group = context.reservation.reservation_id if context.reservation else INVENTORY
+            resource_name = context.resource.name
+        elif is_instance_of(context, 'ResourceRemoteCommandContext'):
+            log_group = context.remote_reservation.reservation_id if context.remote_reservation else INVENTORY
+            resource_name = context.remote_endpoints[0].name
+        else:
+            raise Exception('get_logger_for_context', 'Unsupported command context provided {0}'.format(context))
+
+        exec_info = LoggingSessionContext.get_execution_info(context)
+        qs_logger = get_qs_logger(log_group=log_group, log_category='QS', log_file_prefix=resource_name)
+        log_execution_info(qs_logger, exec_info)
+        return qs_logger
+
+    @staticmethod
+    def get_logger_with_thread_id(context):
+        """
+        Create QS Logger for command context AutoLoadCommandContext, ResourceCommandContext
+        or ResourceRemoteCommandContext with thread name
+        :param context:
+        :return:
+        :rtype: logging.Logger
+        """
+        logger = LoggingSessionContext.get_logger_for_context(context)
+        child = logger.getChild(threading.currentThread().name)
+        for handler in logger.handlers:
+            child.addHandler(handler)
+        child.level = logger.level
+        for log_filter in logger.filters:
+            child.addFilter(log_filter)
+        return child
+
     def __enter__(self):
         """
         Initializes logger for the context
         :return: Logger
         :rtype: logging.Logger
         """
-        self._logger = get_logger_with_thread_id(self.context)
+        self._logger = self.get_logger_with_thread_id(self.context)
         return self._logger
 
     def __exit__(self, exc_type, exc_val, exc_tb):
