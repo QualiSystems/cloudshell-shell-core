e16ca477b9f560f0c4faa75a6aea988cc71a5db9
Kyrylo Maksymenko <saklar13@gmail.com>
2023-05-03 15:44:11 +0200

    add info about the Shell version and whether it's official

--- cloudshell/shell/core/session/logging_session.py
+++ cloudshell/shell/core/session/logging_session.py
@@ -2,8 +2,12 @@ from __future__ import annotations
 
 import platform
 import socket
+from contextlib import suppress
 
 from cloudshell.logging.qs_logger import get_qs_logger
+from cloudshell.rest.api import PackagingRestApiClient
+from cloudshell.rest.exceptions import ShellNotFound
+from cloudshell.rest.models import ShellInfo
 
 from cloudshell.shell.core.context_utils import (
     get_reservation_context_attribute,
@@ -58,6 +62,11 @@ class LoggingSessionContext:
             )
         except Exception:
             pass
+
+        with suppress(Exception):
+            shell = _get_shell(context)
+            reservation_info["Shell Version"] = shell.version
+            reservation_info["Shell Official"] = shell.is_official
 
         installed_packages = tuple(
             f"{name} == {version}"
@@ -142,3 +151,22 @@ class LoggingSessionContext:
         if exc_val:
             self._logger.exception("Error occurred")
         return False
+
+
+def _get_shell(context) -> ShellInfo:
+    rest_api = PackagingRestApiClient(
+        context.connectivity.server_address, context.connectivity.admin_auth_token
+    )
+    model = context.resource.model
+    try:
+        # get shell by model name
+        shell = rest_api.get_shell_as_model(model)
+    except ShellNotFound:
+        # try to add "Shell" in the shell name
+        if model.endswith(" 2G") and not model.endswith(" Shell 2G"):
+            shell_name = model.replace(" 2G", " Shell 2G")
+        else:
+            raise
+        shell = rest_api.get_shell_as_model(shell_name)
+
+    return shell
