d31547b4e5c1e1615d579ba339988feb4bd39952
QUALISYSTEMS\nahum-t <ntimerman@gmail.com>
2023-10-25 16:18:12 +0300

    fix

--- cloudshell/shell/core/driver_context.py
+++ cloudshell/shell/core/driver_context.py
@@ -57,9 +57,11 @@ class ConnectivityContext:
     :type cloudshell_api_scheme: str
 
     :param use_webapi_endpoint: Is Cloudshell API Web Api Endpoint enabled on server? 
-    can be either True or False
-    :type use_webapi_endpoint: bool
+    can be either 'True' or 'False'
+    :type use_webapi_endpoint: str
     """
+
+
 
     def __init__(
         self,
@@ -77,7 +79,7 @@ class ConnectivityContext:
         self.admin_auth_token = admin_auth_token
         self.cloudshell_version = cloudshell_version
         self.cloudshell_api_scheme = cloudshell_api_scheme
-        self.use_webapi_endpoint = True if str(use_webapi_endpoint).lower()=='true' else False
+        self.use_webapi_endpoint = use_webapi_endpoint
 
 
 class ResourceContextDetails:
